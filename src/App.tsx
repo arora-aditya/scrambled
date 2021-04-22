4b81621fe0cc97f1d681d8dbb819c726270d825c
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2021-04-22 11:40:04 -0400

    updated to Scrambled - update CNAME - add scrambled title to page title - add gh-pages - handle deployment on gh-pages using 404.html - add scss for mobile pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import { Helmet } from "react-helmet";
 import { LZMA_WORKER } from "lzma/src/lzma_worker.js";
 import { toByteArray, fromByteArray } from "base64-js";
 import "./App.scss";
@@ -6,6 +7,8 @@ import "./App.scss";
 import TextBox from "./components/textbox"
 
 const DELIMITER = "!@#$%^&*()";
+const HASHED_DELIMITER = "XQAAAAIKAAAAAAAAAAAQkABiQLFNXahyuyIk7f//uSAAAA==";
+const TEMPLATE = "XQAAAAI+AgAAAAAAAAAkGkAHQ3z02RwUSwoZnrEUJg0Z0Iy73ycWbTqb+TsPe7Qn/cedbhHRYxBkZKTZlcaOTcEcC2A0O/wBnTw1y/WX75WkzcZgZhKiXFEekInLXeLagifUS6LpMvp5ABLCW2Kn4Bc6EyCsblKeWj+BsNkcw3yJ95X2w8d9hjpXgRtWlpcwbKk65YKFBhqsMFpD6hvD4xgRDFM/AsfQ2Rra7V3vlE9kJMlTVaoTBh4PBs66hkNkRJmTxSPSnX8CovCH5n/QkO/5tZlxjO6GwIygMJIprg1yRrw2/o+2k2mIANvUcwHlah0hKa0iOnlkH/140ux5JcXLwQkLZggCSCKRvCtBULhTycNDDVmEGKLA4gRU2WY3FwSv4TnhzdBZwVKushFiMgTK5cYlCaSzd8ZqdVF2Xfqbvv899t0AqcFCg2KCRLQaxI+b1JzVahXiBbOaPs7QTxei3Cr2xG6B4kP3kVk/9WvUmAQjBSdqw9dfUQ2RVuU2Wtd3gcqGZLNvC19jS0yD3VrWHQjulIhczIhpIGii7jN700gpnhxVdgQBlk3RXsjpWi3opC5l4Z0T8pbUHtJubwPCvZuy53OK8gJ+lOP+42rn";
 
 function App() {
   let urlParsed = true;
@@ -15,7 +18,6 @@ function App() {
 
   const parseUrl = (url: string | null) => {
     if(url && url.length !== 0){
-      console.log(url);
       const byteArray = toByteArray(url);
       return LZMA_WORKER.decompress(byteArray);
     }
@@ -31,12 +33,19 @@ function App() {
   useEffect(
   () => {
     urlParsed = false;
-    let merged = parseUrl(window.location.pathname.substring(1));
+    let hash = window.location.pathname.substring(1);
+    if(hash === ""){
+      hash = TEMPLATE;
+    }
+    let merged = parseUrl(hash);
     if(merged === DELIMITER){
       const urlParams = new URLSearchParams(window.location.search);
       merged = parseUrl(urlParams.get("q"));
     }
     let [t, ...c] = merged.split(DELIMITER);
+    if (t === "" && c === ""){
+      
+    }
     setContent(c.join(DELIMITER));
     setTitle(t);
   }, []);
@@ -47,9 +56,17 @@ function App() {
     window.history.pushState(null, "", "/" + hash);
   }, [title, content]);
   
+  let pagetitle = "Scrambled";
+  if(title !== ""){
+    pagetitle = `${title} - Scrambled`
+  }
+  
   return (
-    <div className="Xena">
-       <TextBox title={title} setTitle={setTitle} content={content} setContent={setContent}/>
+    <div className="Scrambled">
+        <Helmet>
+            <title>{pagetitle}</title>
+        </Helmet>
+        <TextBox title={title} setTitle={setTitle} content={content} setContent={setContent}/>
     </div>
   );
 }
